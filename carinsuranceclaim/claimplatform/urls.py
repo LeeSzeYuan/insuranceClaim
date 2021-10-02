2eff12ddb8a594b4019d9742c4711348c5ef5249
LeeSzeYuan <lsyuan1029@gmail.com>
2021-10-02 21:11:54 +0800

    added ocr and similarity algorithm

--- carinsuranceclaim/claimplatform/urls.py
+++ carinsuranceclaim/claimplatform/urls.py
@@ -1,9 +1,14 @@
+from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
     path("",views.index,name="index"),
     path("post_create/",views.post_create,name='post_create'),
     path("table/",views.table,name="table")
-    
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
