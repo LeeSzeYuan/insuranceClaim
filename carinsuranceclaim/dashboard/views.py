e2271ad7360c32a7d9c4ea9277692803a60b086d
Khor Chin Yi <CKHOR002@e.ntu.edu.sg>
2021-10-02 18:41:27 +0800

    ham

--- carinsuranceclaim/dashboard/views.py
+++ carinsuranceclaim/dashboard/views.py
@@ -27,13 +27,39 @@ def details(request,claim_no):
     insurance_id=database.child('claim').child(claim_no).child('insurance_id').get().val()
     user_id=database.child('car_insurance').child(insurance_id).child('user_id').get().val()
     NAME=database.child('users').child(user_id).child('name').get().val()
-    INSURANCENUMBER=database.child('vehicle').child('0').child('car_insurance_id').get().val()
+    INSURANCENUMBER=database.child('claim').child(claim_no).child('insurance_id').get().val()
+    Driver_age=database.child('users').child(user_id).child('age').get().val()
+    Driver_phone_number=database.child('users').child(user_id).child('phone_number').get().val()
+    Driver_address=database.child('users').child(user_id).child('address').get().val()
+    Date_of_loss=database.child('claim').child(claim_no).child('date_of_accidence').get().val()
+    Place_of_loss=database.child('claim').child(claim_no).child('accidence_location').get().val()
+    Circumstance=database.child('claim').child(claim_no).child('type_of_accidence').get().val()
+    Registration_number=database.child('vehicle').order_by_child("car_insurance_id").equal_to(INSURANCENUMBER).get().val()['0']['registration_number']
+    Brand=database.child('vehicle').order_by_child("car_insurance_id").equal_to(INSURANCENUMBER).get().val()['0']['brand']
+    Model=database.child('vehicle').order_by_child("car_insurance_id").equal_to(INSURANCENUMBER).get().val()['0']['model']
+    Year=database.child('vehicle').order_by_child("car_insurance_id").equal_to(INSURANCENUMBER).get().val()['0']['year']
 
-    # DATE_OF_=database.child('claim').child('-Mkv6-mXxnfNWYTEfMpq').child('invoice_url').get().val()
+    DATE_OF_=database.child('claim').child('-Mkv6-mXxnfNWYTEfMpq').child('invoice_url').get().val()
     return render(request,"dashboard/details.html",{
       "ClaimNo":ClaimNo,
       "NAME":NAME,
       "CLIENTNUMBER":user_id,
-      "INSURANCENUMBER":INSURANCENUMBER 
+      "INSURANCENUMBER":INSURANCENUMBER,
     #   "DATE_OF_CLAIM": DATE_OF_CLAIM
+      "Driver_name":NAME,
+      "Driver_age":Driver_age,
+      "Driver_phone_number":Driver_phone_number,
+      "Driver_address":Driver_address,
+      "Date_of_loss":Date_of_loss,
+      "Place_of_loss":Place_of_loss,
+      "Circumstance":Circumstance,
+      "Registration_number":Registration_number,
+      "Brand":Brand,
+      "Model":Model,
+      "Year":Year,
+      
+      
+      
+       
+      
     })
